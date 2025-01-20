fd1598e4f88836faaebc8cc88d4fc2a2dcd9b39e
kumarshemant1 <35269303+kumarshemant1@users.noreply.github.com>
2025-01-20 15:09:09 +0530

    Update WarehouseServiceImpl.java

--- src/main/java/app/inventory/service/WarehouseServiceImpl.java
+++ src/main/java/app/inventory/service/WarehouseServiceImpl.java
@@ -11,22 +11,32 @@ import jakarta.validation.constraints.NotNull;
 @Service
 public class WarehouseServiceImpl implements WarehouseService {
 
+	private WarehouseRepository warehouseRepository;
+
+	public WarehouseServiceImpl(WarehouseRepository warehouseRepository) {
+		this.warehouseRepository = warehouseRepository;
+	}
+
 	@Override
 	public Warehouse createWarehouse(@NotNull Warehouse warehouse) {
-		
-		return null;
+		Warehouse response = warehouseRepository.save(warehouse);
+		return response;
 	}
 
 	@Override
 	public Warehouse getWarehouse(@NotNull Long warehouseId) {
-		
-		return null;
+		return warehouseRepository
+			.findById(warehouseId)
+			.orElseThrow(() -> new WarehouseNotFoundException("Warehouse not found with warehouseId : " + warehouseId));
 	}
 
 	@Override
 	public Warehouse updateWarehouse(@NotNull Warehouse warehouse) {
-		
-		return null;
+		Optional<Warehouse> existingObject = getWarehouse(warehouse.getWarehouseId());
+		Warehouse persistObject = new Warehouse();
+		persistObject.setWarehouseId();
+		persistObject.setWarehouseName();
+		return warehouseRepository.save(persistObject);
 	}
 
 	@Override
